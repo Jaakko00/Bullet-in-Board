e293a2db8a1285ba8fc1f831a7a74ff514f77d80
Jaakko Saranpää <jaakko.saranpaa@tuni.fi>
2022-01-04 21:16:16 +0200

    Change imports from locations to posts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 var cors = require("cors");
 const app = express();
 
-const locations = require("./routes/locations.js");
+const posts = require("./routes/posts.js");
 
 app.use(cors());
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("frontend/build"));
 
-app.use("/locations", locations);
+app.use("/posts", posts);
 
 const server = app.listen(8080, () => {
   console.log(`Listening on port ${server.address().port}`);
